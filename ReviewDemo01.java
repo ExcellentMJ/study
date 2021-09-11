260a880aed351e3323f042d1a2ba335e70734b96
5035a <5035als@naver.com>
2021-09-11 17:37:00 +0900

    v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
-// v0.3
+// v0.5
 public class ReviewDemo01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          int number = sc.nextInt();
          boolean isPrime = true;
 
-         for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
-             if(number % k ==0) //입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//         for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
+//             if(number % k ==0) { //입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//                 isPrime = false;
+//                 break; //첫번째 약수가 발견되면 포문 탈출, 성능 향상
+//             }
+//         }
+         int k=2;
+         while(k<number){
+             if((number % k) ==0){
                  isPrime = false;
+                 break;
+             }
+             k++;
          }
          if(isPrime)// isPrime 변수 값이 true이면
              System.out.println(number + "은(는) 소수!");
